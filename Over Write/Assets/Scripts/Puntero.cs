582b7cebc3ebd24500e8e92ff318baf64ace2cea
DanielDiazRuiz2002 <102238701+DanielDiazRuiz2002@users.noreply.github.com>
2023-06-08 16:12:08 -0500

    Nivel de introducción v0.8
    Se continuó con el requerimiento de la escritura del texto, realizando un puntero que indique en que parte de la oración te encuentras

--- Over Write/Assets/Scripts/Puntero.cs
+++ Over Write/Assets/Scripts/Puntero.cs
@@ -3,6 +3,7 @@ using TMPro;
 
 public class Puntero : MonoBehaviour
 {
+    public Transform camara;
     public RectTransform textos;
     public TMP_Text textoEscrito; // Referencia al objeto de texto
 
@@ -18,14 +19,14 @@ public class Puntero : MonoBehaviour
         float widthText = textoEscrito.preferredWidth;
         if (widthText <= 170f)
         {
-            Vector2 targetPosition = new Vector2((25f + widthText) * 5f, transform.position.y);
+            Vector2 targetPosition = new Vector2( camara.transform.position.x - 1015f + (25f + (widthText) * 1.015f) * 5f, transform.position.y);
 
             transform.position = targetPosition;
         }
         else
         {
             // + (widthText) * 5f  - 150f * 5
-            Vector2 targetPosition = new Vector2(widthText , transform.position.y);
+            Vector2 targetPosition = new Vector2(35f + camara.transform.position.x - 1015f + (25f + (widthText) * 1.015f) * 5f + (170f - widthText) * 5f, transform.position.y);
 
             transform.position = targetPosition;
         }
