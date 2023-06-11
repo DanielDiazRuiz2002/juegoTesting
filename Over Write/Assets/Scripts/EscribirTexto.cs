5a282b49524a5fc1b8afb0b0afd6e9bdb11a6256
DanielDiazRuiz2002 <102238701+DanielDiazRuiz2002@users.noreply.github.com>
2023-06-11 00:17:39 -0500

    Nivel de introducción v0.13
    Se trabajo en el requerimiento del enemigo, y la implementación de la tinta

--- Over Write/Assets/Scripts/EscribirTexto.cs
+++ Over Write/Assets/Scripts/EscribirTexto.cs
@@ -8,10 +8,16 @@ public class EscribirTexto : MonoBehaviour
     public TMP_Text TMPComparar;
     public TMP_Text textMeshPro;
     public Color similarColor;
+    public Color noTintColor;
     public Color differentColor;
+    public RectTransform tinta;
+
+    private Vector2 currentPosition;
+    private float posObj;
     // Start is called before the first frame update
     void Start()
     {
+        posObj = 0f;
         textMeshPro.text = "";
     }
 
@@ -31,7 +37,17 @@ public class EscribirTexto : MonoBehaviour
                 }
                 else if (c != '\n' && c != '\r') // Si el carácter no es un salto de línea
                 {
-                    textMeshPro.text += c;
+                    if(c == ' ')
+                    {
+                        textMeshPro.text += c;
+                    }
+                    else if (currentPosition.x - 0.5f > -96f)
+                    {
+                        textMeshPro.text += c;
+
+                        currentPosition = tinta.anchoredPosition;
+                        posObj -= 2.5f;
+                    }
                 }
             }
 
@@ -41,6 +57,10 @@ public class EscribirTexto : MonoBehaviour
             int minLength = Mathf.Min(originalText.Length, compareToText.Length);
 
             textMeshPro.color = similarColor;
+            if (tinta.anchoredPosition.x <= -96f)
+            {
+                textMeshPro.color = noTintColor;
+            }
             for (int i = 0; i < minLength; i++)
             {
                 if (originalText[i] != compareToText[i])
@@ -50,6 +70,34 @@ public class EscribirTexto : MonoBehaviour
                     return;
                 }
             }
+        }
+        if (posObj < currentPosition.x)
+        {
+            //currentPosition.x = currentPosition.x - 10f;
+            float moveSpeed = 3f; // Define la velocidad de movimiento
+            Vector2 targetPosition = new Vector2(posObj, currentPosition.y);
+            currentPosition = Vector2.Lerp(currentPosition, targetPosition, moveSpeed * Time.deltaTime);
+            tinta.anchoredPosition = currentPosition;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("InkWell"))
+        {
+            Vector2 currentPosition = tinta.anchoredPosition;
+
+            currentPosition.x = currentPosition.x + 39f;
+
+            if(currentPosition.x > -3)
+            {
+                currentPosition.x = -3;
+            }
+
+            tinta.anchoredPosition = currentPosition;
+
+            collision.gameObject.SetActive(false);
+            // El objeto ha hecho contacto con el collider del personaje
+            // Realiza las acciones correspondientes aquí
         }
     }
 }
--- Over Write/Assets/Scripts/MovimientoJugador.cs
+++ Over Write/Assets/Scripts/MovimientoJugador.cs
@@ -7,10 +7,18 @@ public class MovimientoJugador : MonoBehaviour
     public float speed;
     // Start is called before the first frame update
     private Rigidbody2D rb;
+    private SpriteRenderer render;
+    public RectTransform tinta;
+
+    private bool choqueEnemigo;
+    private Vector2 currentPosition;
+    private float posObj;
 
     private void Start()
     {
+        posObj = 0;
         rb = GetComponent<Rigidbody2D>();
+        render = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,11 +27,16 @@ public class MovimientoJugador : MonoBehaviour
         // Obtener la entrada del teclado solo si se presionan los botones direccionales
         float moveX = 0f;
         float moveY = 0f;
+        Debug.Log("XD");
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow)) {
             moveX = 1f;
-        if (Input.GetKey(KeyCode.LeftArrow))
+            render.flipX = false;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
             moveX = -1f;
+            render.flipX = true;
+        }
 
         if (Input.GetKey(KeyCode.UpArrow))
             moveY = 1f;
@@ -33,14 +46,26 @@ public class MovimientoJugador : MonoBehaviour
         Vector2 movement = new Vector2(moveX, moveY);
 
         // Reducir la velocidad de movimiento hacia la izquierda
-        if (moveX != 0f)
+        // if (moveX != 0f)
             movement *= 0.35f;
+
+        if (moveX != 0f)
+            movement *= 0.85f;
 
         movement *= speed * Time.fixedDeltaTime;
 
         Vector2 newPosition = rb.position + movement;
 
         rb.MovePosition(newPosition);
+
+        if (posObj < currentPosition.x)
+        {
+            //currentPosition.x = currentPosition.x - 10f;
+            float moveSpeed = 1f; // Define la velocidad de movimiento
+            Vector2 targetPosition = new Vector2(posObj, currentPosition.y);
+            currentPosition = Vector2.Lerp(currentPosition, targetPosition, moveSpeed * Time.deltaTime);
+            tinta.anchoredPosition = currentPosition;
+        }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -51,6 +76,19 @@ public class MovimientoJugador : MonoBehaviour
             currentVelocity *= -1f; // Invertir la velocidad para retroceder
 
             rb.velocity = currentVelocity;
+        }
+
+        float pushForce = 2f;
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Obtener el componente Rigidbody2D del personaje
+
+            // Aplicar una fuerza o impulso hacia X+
+            Vector2 pushVector = new Vector2(pushForce, 0f);
+            rb.AddForce(pushVector, ForceMode2D.Impulse);
+
+            currentPosition = tinta.anchoredPosition;
+            posObj = currentPosition.x - 18f;
         }
     }
 }
